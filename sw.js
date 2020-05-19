e3eeed2bd622dc1f5c0fa133fc518b69a83b06a9
aaarendt <arendta@uw.edu>
2020-05-19 09:16:21 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8c157cc504eda13a7254.js"
+    "url": "webpack-runtime-5ddedd1cbb9babcf0717.js"
   },
   {
     "url": "framework-4f6f95a87e6130d5bbab.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cc7237e6ac2087456b2473c0596cbf4a"
+    "revision": "8de72fd592a4bf83ec34a37939ac44f6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-03961122c56b363d9fd9.js"
