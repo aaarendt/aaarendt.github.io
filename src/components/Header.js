bed96bb8cbd53eb9a603b80fed31dea5b1f3a9e1
aaarendt <arendta@uw.edu>
2021-01-31 15:42:41 -0800

    updates for Feb 27 retreat

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ class Header extends React.Component {
                 <div className="inner">
                     <h1>Male Journeys</h1>
                     <h2>A Courage and Renewal Experience</h2> 
-                    <h4>Retreats, wilderness hikes and online forums to support men's inner journey toward wholeness and community.</h4>
+                    <h3>Retreats, wilderness hikes and online forums to support men's inner journey toward wholeness and community.</h3>
+                    <h4>Join us for our next <a href="retreats">virtual retreat</a>: February 27, 2021</h4>
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Welcome</a></li>
                     </ul>
--- src/pages/retreats.js
+++ src/pages/retreats.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from '../components/layout-pages';
-import pic06 from '../assets/images/support.png' 
+import pic06 from '../assets/images/heart.jpg' 
 
 export default () => (
 <Layout>
@@ -11,35 +11,32 @@ export default () => (
             <h2>Retreat Offerings</h2>
         </header>
         <h3>Male Journeys: Supporting our Spirits in these Challenging Times</h3>
-Since the beginning of the pandemic we have been hosting <a href="../online">regular zoom calls</a> with men who have attended our previous retreats. We found that we were able to share a deep sense of connection despite our physical separation.
-
-Building on these experiences, we will be offering a series of four virtual retreats during early 2021, open to anyone.
+        <p>Jeff and Anthony are hosting two hour long virtual retreats on Saturdays, 10:00 AM to 12:00 PM Pacific Time, on the following dates: February 27, March 27, April 17.</p>
+        <span className="image fit"><img src={pic06} alt="" /></span>
     </div>
     <div className="col-6">
         <br></br>
         <br></br>        
         <br></br>       
         <br></br>
-        Our two hour retreats will occur on Saturdays, 10:00 AM to 12:00 PM Pacific Time, on the following dates: January 30, February 27, March 27, April 17.
         <br></br>
         <br></br>
-        <span className="image fit"><img src={pic06} alt="" /></span>
-        <p><h6>(<a href="https://www.flickr.com/photos/bananer/">Images</a> licensed under a <a href="https://creativecommons.org/licenses/by/2.0/legalcode">Creative Commons Attribution-2.0 Generic licence.</a>)</h6></p>
         <br></br>
-    </div>
-</div>
-<center>
+        <p> During these retreats we offer a hospitable space for men of all ages to show up authentically and live wholeheartedly. We co-create a trustworthy circle in which we can listen to each other with courage and compassion as we find our way together in community. In virtual large group, small group and solitary settings we will invite you to rest, connect with your surroundings and contemplate the poetry, art and stories of wisdom traditions. We welcome anyone identifying as male, regardless of birth assignment. </p>
+        <center>
     <ul className="actions uniform">
-        <li><a href="https://www.eventbrite.com/e/male-journeys-supporting-our-spirits-in-these-challenging-times-tickets-134914614243" className="button special">Register</a></li>
+        <li><a href="https://www.eventbrite.com/e/male-journeys-supporting-our-spirits-in-these-challenging-times-tickets-134914618255" className="button special">Register</a></li>
     </ul>
 </center>
+        
+    </div>
+</div>
+
 </section>
 
 <section id="two" className="main style2">
 <div>
     <center><h2>What can I expect at these retreats?</h2>
-
-    <p> During this retreat we offer a hospitable space for men of all ages to show up authentically and live wholeheartedly. We co-create a trustworthy circle in which we can listen to each other with courage and compassion as we find our way together in community. In virtual large group, small group and solitary settings we will invite you to rest, connect with your surroundings and contemplate the poetry, art and stories of wisdom traditions. We welcome anyone identifying as male, regardless of birth assignment. </p>
 
 
     <p>Jeff has written this <a href={'/Mens-Retreat-Chapter.pdf'} target="_blank">reflection on one of our early in person retreats</a>, describing in detail some of our practices and shared experiences.</p>
