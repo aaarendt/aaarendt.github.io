7a4b638f99882d4258194db55d510c5550cbe429
aaarendt <arendta@uw.edu>
2021-06-12 16:47:59 -0700

    update retreat schedule

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
                     <h1>Male Journeys</h1>
                     <h2>A Courage and Renewal Experience</h2> 
                     <h3>Retreats, wilderness hikes and online forums to support men's inner journey toward wholeness and community.</h3>
-                    <h4>Join us for our next <a href="retreats">virtual retreat</a>: April 17, 2021</h4>
+                    {/*<h4>Join us for our next <a href="retreats">virtual retreat</a>: April 1y7, 2021</h4>*/}
                     <ul className="actions">
                         <li><a href="#one" className="button scrolly">Welcome</a></li>
                     </ul>
